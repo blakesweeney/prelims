2b5220a95fb9eb765289e319d7d26a3ce8478bef
Blake Sweeney <blakes.85@gmail.com>
2013-12-09 09:58:00 -0500

    More calculations

--- neocles/libs/molecular_properties.py
+++ neocles/libs/molecular_properties.py
@@ -7,6 +7,8 @@ import quantities as pq
 pq.piconewton = pq.UnitQuantity('piconewton', (pq.newton * 1e-12).simplified,
                                 symbol='pN')
 pq.piconewtons = pq.piconewton
+pq.kilojoule = pq.UnitQuantity('kilojoule', (pq.joule * 1e3).simplified,
+                               symbol='kJ')
 pq.viscosity.water = 1.00e-3 * pq.pascal * pq.second
 
 T = 310.0 * pq.kelvin
@@ -157,12 +159,22 @@ def compute_viscous_terminal_velocity(friction_coefficient,
 
 
 @output_units(pq.nanometer)
-def compute_viscous_distance(mass, friction_coefficient):
+def compute_viscous_distance(friction_coefficient,
+                             translation_time_constant):
     time = 1 * pq.nanosecond
     force = 1 * pq.piconewton
-    exp = (-1 * time * friction_coefficient / mass).simplified
-    numerator = force * (mass * math.e ** exp + time * friction_coefficient)
-    value = numerator / friction_coefficient ** 2
+    d1 = viscous_distance(time, force, friction_coefficient,
+                          translation_time_constant)
+    d0 = viscous_distance(0 * pq.picosecond, force, friction_coefficient,
+                          translation_time_constant)
+    return d1 - d0
+
+
+def viscous_distance(time, force, friction_coefficient,
+                     translation_time_constant):
+    coeff = (force / friction_coefficient).simplified
+    exp = (-1 * time / translation_time_constant).simplified
+    value = coeff * (translation_time_constant * math.e ** exp + time)
     return value.rescale(pq.nanometer)
 
 
@@ -180,3 +192,94 @@ def compute_ideal_distance(mass):
     time = 1 * pq.picosecond
     value = 0.5 * (force / mass) * time ** 2
     return value.rescale(pq.nanometer)
+
+
+@output_units(pq.joule)
+def compute_thermal_energy():
+    value = (1.0 / 2.0) * pq.constants.Boltzmann_constant * T
+    return value.rescale(pq.joule)
+
+
+@output_units(pq.piconewton * pq.nanometer)
+def compute_molecule_thermal_energy(thermal_energy):
+    return thermal_energy.rescale(pq.piconewton * pq.nanometer)
+
+
+@output_units(pq.joule / pq.mole)
+def compute_atp_per_mole():
+    # CITE: http://en.wikipedia.org/wiki/ATP_hydrolysis
+    return (-30.5 * pq.kilojoule / pq.mole).rescale(pq.joule / pq.mole)
+
+
+@output_units(pq.piconewton * pq.nanometer)
+def compute_atp_per_molecule(atp_per_mole):
+    value = atp_per_mole * pq.mole
+    return value.rescale(pq.piconewton * pq.nanometer)
+
+
+@output_units(pq.dimensionless)
+def compute_atp_kt_mulitples(atp_per_molecule, thermal_energy):
+    return (atp_per_molecule / thermal_energy).simplified
+
+
+@output_units(pq.joule / pq.mole)
+def compute_h_bond_per_mol():
+    # CITE: http://en.wikipedia.org/wiki/Hydrogen_bond
+    return (5 * pq.kilojoule / pq.mole).rescale(pq.joule / pq.mole)
+
+
+@output_units(pq.piconewton * pq.nanometer)
+def compute_h_bond_per_molecule(h_bond_per_mol):
+    value = h_bond_per_mol * pq.mole
+    return value.rescale(pq.piconewton * pq.nanometer)
+
+
+@output_units(pq.dimensionless)
+def compute_h_bond_kt_mulitples(h_bond_per_molecule, thermal_energy):
+    return (h_bond_per_molecule / thermal_energy).simplified
+
+
+@output_units(pq.joule / pq.mole)
+def compute_covalent_bond_energy():
+    # CITE: http://www.science.uwaterloo.ca/~cchieh/cact/c120/bondel.html
+    value = 348 * pq.kilojoule / pq.mole
+    return value.rescale(pq.joule / pq.mole)
+
+
+@output_units(pq.piconewton * pq.nanometer)
+def compute_covalent_bond_per_molecule(covalent_bond_energy):
+    value = covalent_bond_energy * pq.mole
+    return value.rescale(pq.piconewton * pq.nanometer)
+
+
+@output_units(pq.dimensionless)
+def compute_covalent_bond_kt_mulitples(covalent_bond_per_molecule,
+                                       thermal_energy):
+    return (covalent_bond_per_molecule / thermal_energy).simplified
+
+
+@output_units(pq.piconewton)
+def compute_force_in_water():
+    e = 80
+    return electric_force(e, -1 * pq.elementary_charge, pq.elementary_charge,
+                          pq.nanometer)
+
+
+@output_units(pq.piconewton)
+def compute_force_in_protein():
+    e = 3
+    return electric_force(e, -1 * pq.elementary_charge, pq.elementary_charge,
+                          pq.nanometer)
+
+
+@output_units(pq.piconewton)
+def compute_force_in_vacuum():
+    e = 1
+    return electric_force(e, -1 * pq.elementary_charge, pq.elementary_charge,
+                          pq.nanometer)
+
+
+def electric_force(e, q_1, q_2, r):
+    coeff = (1 / (4 * math.pi * pq.constants.electric_constant * e)).simplified
+    ratio = (q_1 * q_2) / r ** 2
+    return (coeff * ratio).rescale(pq.piconewton)
